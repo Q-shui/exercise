8336578dc903fdad3fa734116a9098b71b039b06
Q-shui <73942545+Q-shui@users.noreply.github.com>
2021-04-28 19:43:31 +0800

    Add files via upload

---
+++ exercise_4_28(1)/exercise_4_28(1)/exercise_4_28(1).c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int n = 100;
+//	int i, sum = 0;
+//	for (i = 1; i <= 100; i++)
+//	{
+//		sum += i;
+//	}printf("%d\n", sum);
+//	return 0;
+//}
+int main()
+{
+
+	return 0;
+}
